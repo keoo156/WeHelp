bfd7a95348111c399198d512957e2c1b8cb3542d
keoo156 <keoo156@yahoo.com>
2023-03-31 14:06:15 +0800

    update some code

--- intro/app.js
+++ intro/app.js
@@ -1,21 +1,33 @@
-let allTopics = document.querySelectorAll(".topic")
-allTopics.forEach((topic)=>{
-    topic.addEventListener("click",(e)=>{
-        if(e.target.parentElement.lastElementChild.style.display =="none"){
-            e.target.parentElement.lastElementChild.style.display = "block"
-            e.target.parentElement.lastElementChild.style.animation = "showUp 1.5s ease"
-        }else if (e.target.parentElement.lastElementChild.classList == "inner close"){
-            e.target.parentElement.lastElementChild.classList.remove("close")
-            e.target.parentElement.lastElementChild.style.display = "none"
-
-        }else {
-            e.target.parentElement.lastElementChild.classList.add("close");
-            e.target.parentElement.lastElementChild.addEventListener("transitionend", ()=>{
-                e.target.parentElement.lastElementChild.style.display = "none";
-                e.target.parentElement.lastElementChild.classList.remove("close")
-
-            })
-
-        }
-    });
+let allTopics = document.querySelectorAll(".topic");
+allTopics.forEach((topic) => {
+  topic.addEventListener("click", openInner);
 });
+
+function openInner(e) {
+  if (e.target.parentElement.lastElementChild.style.display == "none") {
+    e.target.removeEventListener("click", openInner);
+    e.target.parentElement.lastElementChild.style.display = "block";
+    e.target.parentElement.lastElementChild.style.animation =
+      "showUp 1.5s ease";
+    e.target.parentElement.addEventListener(
+      "animationend",
+      e.target.addEventListener("click", closeInner)
+    );
+  }
+
+}
+
+function closeInner(e) {
+  if (e.target.parentElement.lastElementChild.style.display == "block") {
+    e.target.parentElement.lastElementChild.classList.add("close");
+    e.target.parentElement.lastElementChild.addEventListener(
+      "transitionend",
+      () => {
+        e.target.parentElement.lastElementChild.style.display = "none";
+        e.target.parentElement.lastElementChild.classList.remove("close");
+        e.target.addEventListener("click", openInner);
+        
+      }
+    );
+  }
+}
